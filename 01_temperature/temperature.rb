b5187d2e045799ddcfd459a7dffd3142d195afe7
mahititi <meroyer@hotmail.fr>
2018-10-08 12:25:19 +0200

    exo_temperature

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
-#write your code here
+def ftoc(fahrenheit_degrees)
+  celsius = (fahrenheit_degrees - 32.0) * 5.0 / 9.0
+  celsius.round
+end
+
+def ctof(celsius_degrees)
+  fahrenheit = (celsius_degrees * 9.0 / 5.0) + 32.0
+  fahrenheit
+end
