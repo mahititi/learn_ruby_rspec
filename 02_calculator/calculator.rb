9a1d49d8d0b40bb7a3f361eae44347e2f6414a4e
mahititi <meroyer@hotmail.fr>
2018-10-08 14:13:04 +0200

    exo_calcul

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
-#write your code here
+def add (a,b)
+return result= a + b
+end
+def subtract (a,b)
+return result= a - b
+end
+
+def sum(numbers)
+  return 0 if numbers.length < 1
+  result = 0
+  numbers.each { |num| result += num }
+  result
+end
+def multiply (a,b)
+  return result= a * b
+end
